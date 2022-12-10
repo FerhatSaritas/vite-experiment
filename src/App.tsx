0bd212cd074a5d8927638feaf2f5b85cb93c5503
Ferhat <ferhat.saritas62@googlemail.com>
2022-12-10 22:03:06 +0100

    Created a simple todo list app

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
-import type { Component } from 'solid-js';
+import { Component, createSignal, createEffect, For} from 'solid-js';
 
-import logo from './logo.svg';
 import styles from './App.module.css';
 
 const App: Component = () => {
+  const [todos, setTodos] = createSignal<string[]>([]);
+  const [newTodo, setNewTodo] = createSignal<string>('');
+
+  const addTodo = (event: Event) => {
+    event.preventDefault();
+    setTodos((todoList) => [...todoList, newTodo()])
+  }
+
+  createEffect(() => console.log(todos()))
+
   return (
     <div class={styles.App}>
       <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
+        <form onSubmit={addTodo}>
+          <input id='todo' type='text' value={newTodo() ?? ''} onChange={(event) => setNewTodo(event.currentTarget.value)} />
+          <button type='submit'>Submit</button>
+        </form>
+        <h2>Meine Todos {todos().length}</h2>
+        <ul>
+          <For each={todos()}>
+            {(item) => <li>{item}</li>}
+          </For>
+        </ul>
       </header>
     </div>
   );
